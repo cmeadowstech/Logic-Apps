a89102345e9f4f1296c28a1b4052e0fb7f927cd2
Cody Meadows <v-cmeadows@microsoft.com>
2023-01-10 13:37:58 -0600

    Add Terraform

---
+++ Terraform/logic-app-standard.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.38.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+variable "prefix" {
+  type    = string
+  default = "la"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg-${random_integer.ri.result}"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.prefix}storage${random_integer.ri.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.prefix}-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "WS1"
+}
+
+resource "azurerm_logic_app_standard" "example" {
+  name                       = "${var.prefix}-la-${random_integer.ri.result}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"     = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "~14"
+  }
+}
